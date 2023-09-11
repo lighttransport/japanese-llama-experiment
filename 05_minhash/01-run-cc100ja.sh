917f5ff596a7134937e9c1eea8587f6c00a8f339
Syoyo Fujita <syoyo@lighttransport.com>
2023-09-12 06:00:38 +0900

    Update minhash script.

--- 05_minhash/01-run-cc100ja.sh
+++ 05_minhash/01-run-cc100ja.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-../cpp/build/cpp_proc minhash ../data/02_clean_step1/cc100ja/ ../data/05_minhash/
+src_dir=/mnt/disk01/work/japanese-dataset-cleaned/02_clean_step1/cc100ja/
+out_dir=/mnt/disk01/work/japanese-dataset-cleaned/05_minhash/
+../cpp/build/cpp_proc minhash ${src_dir} ${out_dir}
