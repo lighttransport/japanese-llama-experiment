599b759f176f784ddb399e6121b1cc8387396d1a
Syoyo Fujita <syoyo@lighttransport.com>
2023-09-23 03:58:41 +0900

    modify filepath

--- 06_dedup/01-run-dedup.sh
+++ 06_dedup/01-run-dedup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 inout_dir=/mnt/disk01/work/japanese-dataset-cleaned-experiment/05_minhash/
-out_dir=/mnt/disk01/work/japanese-dataset-cleaned/06_dedup/
+out_dir=/mnt/disk01/work/japanese-dataset-cleaned-experiment/06_dedup/
 ../cpp/build/cpp_proc dedup ${inout_dir} ${out_dir}
