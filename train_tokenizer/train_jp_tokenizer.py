0850165b734f6f550f88a358d0fb90e60ac4a7a6
Syoyo Fujita <syoyo@lighttransport.com>
2023-10-15 22:12:30 +0900

    Use wikipedia dataset to train jp tokenizer.

--- train_tokenizer/train_jp_tokenizer.py
+++ train_tokenizer/train_jp_tokenizer.py
@@ -1,28 +1,35 @@
 # NOTE: 128 GB CPU mem is required.
 from tokenizers import Tokenizer
-from tokenizers.models import BPE
+from tokenizers.models import Unigram
 from tokenizers import normalizers
-from tokenizers.trainers import BpeTrainer
+from tokenizers.trainers import UnigramTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from datasets import load_dataset
 
-tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+
+dataset_name = "range3/wikipedia-ja-20230101"
+
+tokenizer = Tokenizer(Unigram())
 tokenizer.pre_tokenizer = Whitespace()
 tokenizer.normalizer = normalizers.NFKC()
 
 # TODO: Use [BOS], [EOS] instead of [CLS], [SEP]?
 # NOTE: Chinese LLaMa uses vocab_size=20000
-trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"], vocab_size=30000)
+trainer = UnigramTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"], unk_token="<UNK>", vocab_size=30000)
 
-dataset = load_dataset('range3/cc100-ja')
+dataset = load_dataset(dataset_name)
 
 def dataset_iter():
+    # for cc100-ja
     # roughly 700MB
     # reducing `skip` will cause OOM if you have less than 128 GB CPU mem.
-    skip=100
+    # skip=100
+
+    # for wikipedia
+    skip=1
     for i in range(0, len(dataset['train']), skip):
         yield dataset['train'][i]['text']
 
 tokenizer.train_from_iterator(dataset_iter(), trainer)
-tokenizer.save('data/tokenizer-cc100-ja.json')
+tokenizer.save('data/tokenizer-wikipedia-ja.json')
 
