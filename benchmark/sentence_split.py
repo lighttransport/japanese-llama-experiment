85364309a8c783a2d823d7c5612f7cf7f3579000
Syoyo Fujita <syoyo@lighttransport.com>
2024-01-16 22:42:35 +0900

    support multiple models.

--- benchmark/sentence_split.py
+++ benchmark/sentence_split.py
@@ -6,17 +6,38 @@ from common_util import load_jsonl_zstd
 
 jsons = load_jsonl_zstd.load("../data/00_dataset/cc100/cc100-ja.00000.jsonl.zstd")
 
-import tqdm
+import concurrent.futures
+from multiprocessing import cpu_count
+
+from tqdm import tqdm
 import spacy
-spacy.prefer_gpu()
-nlp = spacy.load('ja_ginza_electra')
+
+model_name = 'ja_core_news_md'
+#model_name = 'ja_ginza'
+# NOTE: ja_ginza_electra hands in Python multiprocessing environment
+#model_name = 'ja_ginza_electra'
+
+if model_name == 'ja_ginza_electra':
+    spacy.prefer_gpu()
+
+nlp = spacy.load(model_name)
 
 num_batch = 100
 
+num_process = max(1, cpu_count() // 4)
+
+def proc(texts):
+    docs = list(nlp.pipe(texts, disable=['ner', 'bunsetu_recognizer']))
+
+    for doc in docs:
+        for sent in doc.sents:
+            print(sent)
+
+    return None
+
 texts = []
-for j in tqdm.tqdm(jsons):
+for j in tqdm(jsons):
     text = j['text']
-    #print(text)
     
     kMaxLen = 15000 # NOTE: 49149 bytes = Sudachi's limitation
 
@@ -25,12 +46,22 @@ for j in tqdm.tqdm(jsons):
 
     texts.append(text)
 
-    if len(texts) >= num_batch:
+    if model_name == 'ja_ginza_electra':
+        if len(texts) >= num_batch:
+            proc(texts)
 
-        docs = list(nlp.pipe(texts, disable=['ner', 'bunsetu_recognizer']))
-        for doc in docs:
-            for sent in doc.sents:
-                print(sent)
-        
-        texts = []
+            texts = []
+
+# multiprocessing
+if model_name != 'ja_ginza_electra':
+    total_ticks = max(1, len(texts) // num_batch)
+    with tqdm(total=total_ticks) as pbar:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_process) as executor:
+            futures = {executor.submit(proc, texts[i:i+num_batch]): i for i in range(0, len(texts), num_batch)}
+      
+            results = {}
+            for future in concurrent.futures.as_completed(futures):
+                arg = futures[future]
+                result = future.result()
+                pbar.update(1)
 
