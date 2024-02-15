13bda120fbe9c739d2ecefa8141fa622353e5b44
Syoyo Fujita <syoyo@lighttransport.com>
2024-02-16 05:21:22 +0900

    longopt

--- exact_dedup_at_scale/main.cc
+++ exact_dedup_at_scale/main.cc
@@ -20,5 +20,33 @@ class ResourcePool
 };
 
 int main(int argc, char **argv) {
+
+  struct optparse_long longopts[] = {
+        {"tokenize", 't', OPTPARSE_NONE},
+        {0}
+    };
+
+  bool tokenize{false};
+
+  int option;
+  struct optparse options;
+  optparse_init(&options, argv);
+
+  while((option =  optparse_long(&options, longopts, nullptr)) != -1) {
+    switch (option) {
+      case 't':
+        tokenize = true;
+        break;
+      case '?':
+        fprintf(stderr, "%s: %s\n", argv[0], options.errmsg);
+        exit(EXIT_FAILURE); 
+      }
+  }
+
+  char *arg;
+  while ((arg = optparse_arg(&options))) {
+    printf("%s\n", arg);
+  }
+
   return EXIT_SUCCESS;
 }
